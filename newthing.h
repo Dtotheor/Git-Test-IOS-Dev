c668947edf7b40801b9dc61e74f96428725b00b6
dpeled <dpeled@barneys.com>
2015-08-06 20:53:27 -0400

    addign new file

---
+++ newthing.h
@@ -0,0 +1,13 @@
+//
+//  newthing.h
+//  Git Test
+//
+//  Created by Doreen Peled on 8/6/15.
+//  Copyright (c) 2015 Doreen Peled. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface newthing : NSObject
+
+@end
---
+++ newthing.m
@@ -0,0 +1,13 @@
+//
+//  newthing.m
+//  Git Test
+//
+//  Created by Doreen Peled on 8/6/15.
+//  Copyright (c) 2015 Doreen Peled. All rights reserved.
+//
+
+#import "newthing.h"
+
+@implementation newthing
+
+@end
