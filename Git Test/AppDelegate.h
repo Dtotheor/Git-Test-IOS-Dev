93950b5f1515c3d3507e45d6082af5ca9bfc99f3
dpeled <dpeled@barneys.com>
2015-08-06 20:05:21 -0400

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Doreen Peled on 8/6/15.
+//  Copyright (c) 2015 Doreen Peled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Doreen Peled on 8/6/15.
+//  Copyright (c) 2015 Doreen Peled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
